9398835d420ea5effd9cb5b0321c288fc6aacece
Max Brauer <ma.brauer@live.de>
2022-05-31 10:45:57 +0200

    update examples

--- example/MaxLib.WebServer.Example/Program.cs
+++ example/MaxLib.WebServer.Example/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
 using MaxLib.WebServer.Services;
 
 namespace MaxLib.WebServer.Example
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             WebServerLog.LogAdded += WebServerLog_LogAdded;
-            var server = new Server(new WebServerSettings(8000, 5000));
+            using var server = new Server(new WebServerSettings(8000, 5000));
             // add services
             server.AddWebService(new HttpRequestParser());
             server.AddWebService(new HttpHeaderSpecialAction());
             server.AddWebService(new Http404Service());
             server.AddWebService(new HttpResponseCreator());
             server.AddWebService(new HttpSender());
-            // start server
-            server.Start();
+            // run server until cancel received
+            await server.RunAsync();
         }
 
         private static void WebServerLog_LogAdded(ServerLogItem item)
--- example/MaxLib.WebServer.WebSocket.Echo/Program.cs
+++ example/MaxLib.WebServer.WebSocket.Echo/Program.cs
@@ -7,26 +7,21 @@ namespace MaxLib.WebServer.WebSocket.Echo
 {
     class Program
     {
-        static void Main()
+        static async Task Main()
         {
             WebServerLog.LogAdded += WebServerLog_LogAdded;
-            var server = new Server(new WebServerSettings(8000, 5000));
+            using var server = new Server(new WebServerSettings(8000, 5000));
             // add services
             server.AddWebService(new HttpRequestParser());
             server.AddWebService(new HttpHeaderSpecialAction());
             server.AddWebService(new HttpResponseCreator());
             server.AddWebService(new HttpSender());
             // setup web socket
-            var websocket = new WebSocketService();
+            using var websocket = new WebSocketService();
             websocket.Add(new EchoEndpoint());
             server.AddWebService(websocket);
-            // start server
-            server.Start();
-            // wait for console quit
-            while (Console.ReadKey().Key != ConsoleKey.Q) ;
-            // close
-            server.Stop();
-            websocket.Dispose();
+            // run server until cancel received
+            await server.RunAsync();
         }
 
         private static void WebServerLog_LogAdded(ServerLogItem item)
